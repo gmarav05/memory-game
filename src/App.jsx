acd8429010a1a59084aaca8f93973b0937ac8cd9
Aravind <gmarav005@gmail.com>
2025-12-19 20:08:48 +0530

    Add Fetch from API

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,23 @@ import MemoryCard from './components/MemoryCard'
 export default function App() {
     const [isGameOn, setIsGameOn] = useState(false)
     
-    function startGame(e) {
+    async function startGame(e) {
         e.preventDefault()
-        setIsGameOn(true)
+        
+        try {
+            const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
+            
+            if (!response.ok) {
+                throw new Error("Could not fetch data from API")
+            }
+            
+            const data = await response.json()
+            
+            setIsGameOn(true)
+            console.log(data)
+        } catch(err) {
+            console.error(err)
+        }   
     }
     
     function turnCard() {
